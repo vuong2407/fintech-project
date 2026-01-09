e1317b52192238decac0fbf97fe3cb9afca2666e
Nguyen Vuong <vanvuong24072001@gmail.com>
2026-01-09 21:16:21 +0700

    initial commit

---
+++ src/main/java/com/vuongnguyen/fintech_project/FintechProjectApplication.java
@@ -0,0 +1,13 @@
+package com.vuongnguyen.fintech_project;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FintechProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FintechProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vuongnguyen/fintech_project/FintechProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vuongnguyen.fintech_project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FintechProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
